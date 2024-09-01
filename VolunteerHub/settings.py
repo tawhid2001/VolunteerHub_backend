c9f11cdfd4a0172a19721f08c37ab30444025bd0
tawhid2001 <tawhid.talal@gmail.com>
2024-09-01 15:56:05 -0400

    csrf trusted origin added

--- VolunteerHub/settings.py
+++ VolunteerHub/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://volunteerhub-backend-zlno.onrender.com",
     "http://127.0.0.1:8000",
     'http://127.0.0.1:5500',
     'http://localhost:5500',
