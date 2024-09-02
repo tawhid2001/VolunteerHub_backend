d706de022dfad835a0fa1221a3681e671fc93b7e
tawhid2001 <tawhid.talal@gmail.com>
2024-09-02 12:28:43 -0400

    cors middleware added

--- VolunteerHub/settings.py
+++ VolunteerHub/settings.py
@@ -86,6 +86,7 @@ REST_AUTH_SERIALIZERS = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
