4387a8512101d3104668a3942b91f683d8d35809
tawhid2001 <tawhid.talal@gmail.com>
2024-10-04 11:39:47 -0400

    console email

--- VolunteerHub/settings.py
+++ VolunteerHub/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
